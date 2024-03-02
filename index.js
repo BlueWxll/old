dcbb44d375cfe13e452c54d0252e0cd1dc2268f6
BlueWxll <141902033+BlueWxll@users.noreply.github.com>
2024-03-02 09:34:58 +0100

    Added audio, copyrights
    BlueWxll

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-new snowflakeCursor({ element: document.querySelector("#snowflake") })
+
 window.addEventListener('load', function(){
     var newVideo = document.getElementById('background');
+    var CTA = this.document.getElementsByClassName('access')
+    
+    //newVideo.play();
+
+});
+function access(){
+    var newVideo = document.getElementById('background');
+    var newAudio = document.getElementById('audioSound');
     newVideo.addEventListener('ended', function() {
         this.currentTime = 0;
         this.play();
     }, false);
-
     newVideo.play();
-
-});
+    newAudio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+    newAudio.play();
+    new snowflakeCursor({ element: document.querySelector("#snowflake") });
+    var CTA = this.document.getElementById("access");
+    CTA.remove();
+}
